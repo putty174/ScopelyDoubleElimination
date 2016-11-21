e76a8b3d0bc94db88ce65389daf1b0742edc708a
putty174 <myh10232223@yahoo.com>
2016-11-20 18:58:27 -0800

    Reset Dialog
    Reset Dialog appears after completed bracket.

--- ScopelyDoubleElimination/app/src/main/java/com/example/maxh1/scopelydoubleelimination/MainActivity.java
+++ ScopelyDoubleElimination/app/src/main/java/com/example/maxh1/scopelydoubleelimination/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.maxh1.scopelydoubleelimination;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +30,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        InitializeBracket();
+    }
+
+    protected void PlayButtonClick(View v) {
+        if (bracketArray != null && roundIndex < bracketRounds.size() - 1) {
+            String team1URL = bracketRounds.get(roundIndex).get(teamSetIndex + 0).optString("image");
+            String team2URL = bracketRounds.get(roundIndex).get(teamSetIndex + 1).optString("image");
+
+            Intent pickTeamIntent = new Intent(this, TeamSelect.class);
+            pickTeamIntent.putExtra("team1logo", team1URL);
+            pickTeamIntent.putExtra("team2logo", team2URL);
+            startActivityForResult(pickTeamIntent, PICK_TEAM);
+        }
+        else {
+            showResetDialog();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_TEAM) {
+            if (resultCode == RESULT_OK) {
+                switch (data.getIntExtra("winningteam", 0)) {
+                    case R.id.team1logo:
+                        bracketRounds.get(roundIndex + 1).add(bracketRounds.get(roundIndex).get(teamSetIndex + 0));
+                        teamSetIndex += 2;
+                        break;
+                    case R.id.team2logo:
+                        bracketRounds.get(roundIndex + 1).add(bracketRounds.get(roundIndex).get(teamSetIndex + 1));
+                        teamSetIndex += 2;
+                        break;
+                }
+                int id = getResources().getIdentifier(String.format("round%d", roundIndex + 2), "id", this.getPackageName());
+                TextView nextRound = (TextView) findViewById(id);
+                nextRound.setText("");
+                for (JSONObject winningTeam : bracketRounds.get(roundIndex + 1)) {
+                    nextRound.setText(nextRound.getText() + winningTeam.optString("name") + "\n");
+                }
+
+                if (teamSetIndex >= bracketRounds.get(roundIndex).size()) {
+                    teamSetIndex = 0;
+                    roundIndex++;
+                    if (roundIndex >= bracketRounds.size() - 1) {
+                        showResetDialog();
+                    }
+                }
+            }
+        }
+    }
+
+    private void InitializeBracket() {
+        bracketArray = null;
+        roundIndex = 0;
+        teamSetIndex = 0;
+        bracketRounds = new ArrayList<ArrayList<JSONObject>>();
+
         String JSONString = "";
         URLDownload JSONfile = new URLDownload(MainActivity.this);
         try {
@@ -34,19 +93,26 @@ public class MainActivity extends AppCompatActivity {
             JSONObject bracketJSON = new JSONObject(JSONString);
             bracketArray = bracketJSON.optJSONArray("data");
 
-            for(int roundSize = bracketArray.length(); roundSize > 0; roundSize /= 2)
-            {
+            for (int roundSize = bracketArray.length(); roundSize > 0; roundSize /= 2) {
                 bracketRounds.add(new ArrayList<JSONObject>(roundSize));
             }
 
-            TextView team1 = (TextView) findViewById(R.id.round1);
-            team1.setText("");
-            for(int teamIndex = 0; teamIndex < bracketArray.length(); teamIndex++) {
+            for (int teamIndex = 0; teamIndex < bracketArray.length(); teamIndex++) {
                 bracketRounds.get(roundIndex).add(bracketArray.getJSONObject(teamIndex));
             }
             Collections.shuffle(bracketRounds.get(roundIndex));
+
+            TextView round1 = (TextView) findViewById(R.id.round1);
+            round1.setText("");
+            TextView round2 = (TextView) findViewById(R.id.round2);
+            round2.setText("");
+            TextView round3 = (TextView) findViewById(R.id.round3);
+            round3.setText("");
+            TextView round4 = (TextView) findViewById(R.id.round4);
+            round4.setText("");
+
             for (JSONObject team : bracketRounds.get(roundIndex)) {
-                team1.setText(team1.getText() + team.optString("name") + "\n");
+                round1.setText(round1.getText() + team.optString("name") + "\n");
             }
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
@@ -57,43 +123,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void PlayButtonClick(View v) {
-        if (bracketArray != null && roundIndex < bracketRounds.size() - 2) {
-            if (teamSetIndex >= bracketRounds.get(roundIndex).size()) {
-                teamSetIndex = 0;
-                roundIndex++;
+    private void showResetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Team X Wins\nReset bracket?").setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                InitializeBracket();
             }
-            String team1URL = bracketRounds.get(roundIndex).get(teamSetIndex + 0).optString("image");
-            String team2URL = bracketRounds.get(roundIndex).get(teamSetIndex + 1).optString("image");
-
-            Intent pickTeamIntent = new Intent(this, TeamSelect.class);
-            pickTeamIntent.putExtra("team1logo", team1URL);
-            pickTeamIntent.putExtra("team2logo", team2URL);
-            startActivityForResult(pickTeamIntent, PICK_TEAM);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PICK_TEAM) {
-            if(resultCode == RESULT_OK) {
-                switch (data.getIntExtra("winningteam", 0)) {
-                    case R.id.team1logo:
-                        bracketRounds.get(roundIndex+1).add(bracketRounds.get(roundIndex).get(teamSetIndex+0));
-                        teamSetIndex += 2;
-                        break;
-                    case R.id.team2logo:
-                        bracketRounds.get(roundIndex+1).add(bracketRounds.get(roundIndex).get(teamSetIndex+1));
-                        teamSetIndex += 2;
-                        break;
-                }
-                int id = getResources().getIdentifier(String.format("round%d", roundIndex+2), "id", this.getPackageName());
-                TextView nextRound = (TextView) findViewById(id);
-                nextRound.setText("");
-                for(JSONObject winningTeam : bracketRounds.get(roundIndex+1)) {
-                    nextRound.setText(nextRound.getText() + winningTeam.optString("name") + "\n");
-                }
+        }).setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
             }
-        }
+        });
+        builder.create();
+        builder.show();
     }
 }
