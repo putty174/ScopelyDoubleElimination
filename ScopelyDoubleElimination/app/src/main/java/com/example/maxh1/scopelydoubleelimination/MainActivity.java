725c9112058d406d414419373869f3b0e6a9b509
putty174 <myh10232223@yahoo.com>
2016-11-20 18:11:30 -0800

    Promote Teams to next Round
    Selecting team from Play screen promotes winning team to next round.

--- ScopelyDoubleElimination/app/src/main/java/com/example/maxh1/scopelydoubleelimination/MainActivity.java
+++ ScopelyDoubleElimination/app/src/main/java/com/example/maxh1/scopelydoubleelimination/MainActivity.java
@@ -9,14 +9,18 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     static final int PICK_TEAM = 100;
 
     private JSONArray bracketArray = null;
+    private int roundIndex = 0;
+    private int teamSetIndex = 0;
+    private ArrayList<ArrayList<JSONObject>> bracketRounds = new ArrayList<ArrayList<JSONObject>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +34,20 @@ public class MainActivity extends AppCompatActivity {
             JSONObject bracketJSON = new JSONObject(JSONString);
             bracketArray = bracketJSON.optJSONArray("data");
 
-            TextView team1 = (TextView) findViewById(R.id.team1);
-            team1.setText("");
-            for(int teamIndex = 0; teamIndex < bracketArray.length(); teamIndex++) {
-                team1.setText(team1.getText() + bracketArray.getJSONObject(teamIndex).optString("name") + "\n");
+            for(int roundSize = bracketArray.length(); roundSize > 0; roundSize /= 2)
+            {
+                bracketRounds.add(new ArrayList<JSONObject>(roundSize));
             }
 
+            TextView team1 = (TextView) findViewById(R.id.round1);
+            team1.setText("");
+            for(int teamIndex = 0; teamIndex < bracketArray.length(); teamIndex++) {
+                bracketRounds.get(roundIndex).add(bracketArray.getJSONObject(teamIndex));
+            }
+            Collections.shuffle(bracketRounds.get(roundIndex));
+            for (JSONObject team : bracketRounds.get(roundIndex)) {
+                team1.setText(team1.getText() + team.optString("name") + "\n");
+            }
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
@@ -46,18 +58,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void PlayButtonClick(View v) {
-        if(bracketArray != null) {
-            try {
-                String team1URL = bracketArray.getJSONObject(0).optString("image");
-                String team2URL = bracketArray.getJSONObject(1).optString("image");
-
-                Intent pickTeamIntent = new Intent(this, TeamSelect.class);
-                pickTeamIntent.putExtra("team1logo", team1URL);
-                pickTeamIntent.putExtra("team2logo", team2URL);
-                startActivityForResult(pickTeamIntent, PICK_TEAM);
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
+        if (bracketArray != null && roundIndex < bracketRounds.size() - 2) {
+            if (teamSetIndex >= bracketRounds.get(roundIndex).size()) {
+                teamSetIndex = 0;
+                roundIndex++;
             }
+            String team1URL = bracketRounds.get(roundIndex).get(teamSetIndex + 0).optString("image");
+            String team2URL = bracketRounds.get(roundIndex).get(teamSetIndex + 1).optString("image");
+
+            Intent pickTeamIntent = new Intent(this, TeamSelect.class);
+            pickTeamIntent.putExtra("team1logo", team1URL);
+            pickTeamIntent.putExtra("team2logo", team2URL);
+            startActivityForResult(pickTeamIntent, PICK_TEAM);
         }
     }
 
@@ -65,8 +77,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PICK_TEAM) {
             if(resultCode == RESULT_OK) {
-                TextView team2 = (TextView) findViewById(R.id.team2);
-                team2.setText(Integer.toString(data.getIntExtra("winningteam", 0)));
+                switch (data.getIntExtra("winningteam", 0)) {
+                    case R.id.team1logo:
+                        bracketRounds.get(roundIndex+1).add(bracketRounds.get(roundIndex).get(teamSetIndex+0));
+                        teamSetIndex += 2;
+                        break;
+                    case R.id.team2logo:
+                        bracketRounds.get(roundIndex+1).add(bracketRounds.get(roundIndex).get(teamSetIndex+1));
+                        teamSetIndex += 2;
+                        break;
+                }
+                int id = getResources().getIdentifier(String.format("round%d", roundIndex+2), "id", this.getPackageName());
+                TextView nextRound = (TextView) findViewById(id);
+                nextRound.setText("");
+                for(JSONObject winningTeam : bracketRounds.get(roundIndex+1)) {
+                    nextRound.setText(nextRound.getText() + winningTeam.optString("name") + "\n");
+                }
             }
         }
     }
